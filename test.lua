f690bd12b95b326ee9788613bc774810b9d520dd
St_Pechenka <65812037+Anni003@users.noreply.github.com>
2022-10-31 03:30:10 +0300

    Delete test.lua

--- test.lua
+++
@@ -1,9 +0,0 @@
-local composer = require("composer");
-local scene = composer.newScene(); -- создаём новую сцену
-function scene:show(event)
-    local backgroud = display.newImageRect("white.jpg", display.contentWidth, display.contentHeight)
-    backgroud.x = display.contentCenterX
-    backgroud.y = display.contentCenterY
-end
-scene:addEventListener("show", scene);
-return scene;
